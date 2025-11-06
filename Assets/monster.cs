86b04821d0a70abc6403656a81eeb95f021b8fb7
Linanoi <clownlele51@gmail.com>
2025-11-06 17:35:04 +0300

    lina
    new

---
+++ Assets/monster.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyAi : MonoBehaviour
+{
+    private enum State { Patrol, Chase, Attack}
+    private State currentState = State.Patrol;
+
+    [SerializeField] Transform[] patrolPoints;
+    [SerializeField] Transform player;
+    [SerializeField] float chaseDistance = 5f;
+    [SerializeField] float attackDistance = 2f;
+
+    private NavMeshAgent agent;
+    private AudioSource audioSource;
+    private Animator anim;
+    private int currentIndex = 0;
+
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        Patrol();
+    }
+
+    void Update()
+    {
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+
+        Debug.Log(currentState);
+
+        switch (currentState)
+        {
+            case State.Patrol:
+                Patrol();
+                agent.speed = 2f;
+                if (distanceToPlayer <= chaseDistance)
+                    currentState = State.Chase;
+                break;
+            case State.Chase:
+                Chase();
+                agent.speed = 4f;
+                if (distanceToPlayer <= attackDistance)
+                    currentState = State.Attack;
+                else
+                    currentState = State.Patrol;
+                break;
+            case State.Attack:
+                Attack();
+                if (distanceToPlayer > attackDistance)
+                    currentState = State.Chase;
+                break;
+
+        }
+    }
+
+    void Patrol()
+    {
+        agent.isStopped = false;
+        if (patrolPoints.Length == 0) return;
+        if (!agent.pathPending && agent.remainingDistance < 0.8f)
+        {
+            agent.SetDestination(patrolPoints[currentIndex].position);
+            currentIndex = (currentIndex + 1) % patrolPoints.Length;
+        }
+
+    }
+
+    void Chase()
+    {
+        agent.isStopped = false;
+        audioSource.Play();
+        agent.SetDestination(player.position);
+    }
+
+    void Attack()
+    {
+        //Attack logic
+        agent.isStopped = true;
+        anim.SetTrigger("isAttacking");
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
+    }
+}
